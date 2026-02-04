ffd47e92ba410bcea2449e138a21e342b1d48289
Dev Madlani <madlanidev@gmail.com>
2026-02-04 20:27:21 +0530

    add validator

---
+++ src/category/category-controller.ts
@@ -0,0 +1,7 @@
+import { NextFunction, Request, Response } from 'express'
+
+export class CategoryController {
+    async create(req: Request, res: Response, next: NextFunction) {
+        res.json({})
+    }
+}
